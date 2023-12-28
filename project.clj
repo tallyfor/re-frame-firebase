a3714c5a567f5db740286516fc660d98556dfaad
Alan Shaw <alan@nodename.com>
2023-12-27 17:10:53 -0800

    added flow dependency; Version 0.10.4

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
 ;;; Author: David Goldfarb (deg@degel.com)
 ;;; Copyright (c) 2017-8, David Goldfarb
 
-(defproject tallyfor/re-frame-firebase "0.10.3"
+(defproject tallyfor/re-frame-firebase "0.10.4"
   :description "A re-frame wrapper around firebase"
   :url "https://github.com/deg/re-frame-firebase"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
+                 [tallyfor/flow "4.2.0-NODENAME"]
                  [cljsjs/firebase "7.5.0-0"] ;;"5.7.3-1"
                  [re-frame "0.10.6"]
                  [com.degel/iron "0.4.0"]
-                 [lein-pprint             "1.3.2"]
+                 [lein-pprint "1.3.2"]
                  [lein-cljsbuild "1.1.8"]
                  [lein-bump-version "0.1.6"]]
   ;; run lein install with LEIN_SNAPSHOTS_IN_RELEASE=true lein install
@@ -21,9 +22,9 @@
   :cljsbuild {:builds {}} ; prevent https://github.com/emezeske/lein-cljsbuild/issues/413
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.5.6"]]}
-  :profiles {:dev      {:dependencies [[clj-stacktrace "0.2.8"]
-                                       [binaryage/devtools "0.9.10"]
-                                       [org.clojure/tools.namespace "1.1.0"]]}}
+  :profiles {:dev {:dependencies [[clj-stacktrace "0.2.8"]
+                                  [binaryage/devtools "0.9.10"]
+                                  [org.clojure/tools.namespace "1.1.0"]]}}
   :source-paths ["src" "target/classes"]
   ;; Change your environment variables (maybe editing .zshrc or .bashrc) to have:
   ;; export LEIN_USERNAME="pdelfino"
@@ -31,9 +32,9 @@
   ;; LEIN_PASSWORD should use the same Token used by .npmrc
   ;; Also, do "LEIN_SNAPSHOTS_IN_RELEASE=true lein install" or edit your .zshrc:
   ;; export LEIN_SNAPSHOTS_IN_RELEASE=true
-  :repositories {"releases"  {:url           "https://maven.pkg.github.com/tallyfor/*"
-                              :username      :env/LEIN_USERNAME ;; change your env
-                              :password      :env/LEIN_PASSWORD}}
+  :repositories {"releases" {:url      "https://maven.pkg.github.com/tallyfor/*"
+                             :username :env/LEIN_USERNAME ;; change your env
+                             :password :env/LEIN_PASSWORD}}
 
   :pom-addition [:distribution-management [:repository [:id "github"]
                                            [:name "GitHub Packages"]
